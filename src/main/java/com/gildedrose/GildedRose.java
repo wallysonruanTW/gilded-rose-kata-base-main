2b16bc9954c832be97fc165292700b7c5e7ea1db
wallysonruanTW <wallyson.ruan@thoughtworks.com>
2023-02-18 23:38:08 -0300

    REFACTOR: Isolei os if do AGED BRIE

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -31,27 +31,37 @@ class GildedRose {
 
                     item.sellIn -= 1;
                 }else{
-                    if (!item.name.equals("Aged Brie")) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
+                    if (item.name.equals("Aged Brie")){
+                        item.sellIn -= 1;
+
+                        if (item.sellIn < 0){
+                            item.quality += 2;
+                        }else{
+                            item.quality += 1;
                         }
-                    } else {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        } else {
-                            item.quality = 50;
-                        }
-                    }
-                    item.sellIn = item.sellIn - 1;
-                    if (item.sellIn < 0) {
-                        if (item.name.equals("Aged Brie") && item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        } else {
+
+                    }else {
+                        if (!item.name.equals("Aged Brie")) {
                             if (item.quality > 0) {
                                 item.quality = item.quality - 1;
                             }
+                        } else {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            } else {
+                                item.quality = 50;
+                            }
                         }
-
+                        item.sellIn = item.sellIn - 1;
+                        if (item.sellIn < 0) {
+                            if (item.name.equals("Aged Brie") && item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            } else {
+                                if (item.quality > 0) {
+                                    item.quality = item.quality - 1;
+                                }
+                            }
+                        }
                     }
                 }
             }
