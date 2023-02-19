2ebaa12856d1db675a747c39ab05a6570e41d0ec
wallysonruanTW <wallyson.ruan@thoughtworks.com>
2023-02-18 23:48:34 -0300

    REFACTOR: Isolei os IF de todos os produtos atuais

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -33,34 +33,28 @@ class GildedRose {
                 }else{
                     if (item.name.equals("Aged Brie")){
                         item.sellIn -= 1;
-
                         if (item.sellIn < 0){
                             item.quality += 2;
                         }else{
                             item.quality += 1;
                         }
-
-                    }else {
-                        if (!item.name.equals("Aged Brie")) {
-                            if (item.quality > 0) {
-                                item.quality = item.quality - 1;
-                            }
-                        } else {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            } else {
-                                item.quality = 50;
-                            }
+                        if (item.quality > 50){
+                            item.quality = 50;
                         }
-                        item.sellIn = item.sellIn - 1;
-                        if (item.sellIn < 0) {
-                            if (item.name.equals("Aged Brie") && item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            } else {
-                                if (item.quality > 0) {
-                                    item.quality = item.quality - 1;
-                                }
-                            }
+                    }else {
+                        item.sellIn -= 1;
+
+                        if (item.quality > 50){
+                            item.quality = 50;
+                        }
+                        if (item.sellIn < 0){
+                            item.quality -= 1;
+                        }
+
+                        item.quality -= 1;
+
+                        if (item.quality < 0){
+                            item.quality = 0;
                         }
                     }
                 }
