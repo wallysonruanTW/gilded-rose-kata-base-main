1206ce920499b5bd4b15e1e202364379f8922dcb
wallysonruanTW <wallyson.ruan@thoughtworks.com>
2023-02-18 23:35:03 -0300

    REFACTOR: Isolei os if relacionados ao concerto

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,32 +10,40 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            }else{
+                if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
+                    if (item.sellIn > 5 && item.sellIn <= 10) {
+                        item.quality += 2;
                     }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-
-                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.sellIn < 11) {
-                                item.quality = item.quality + 1;
-                            }
-                            if (item.sellIn < 6) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    } else {
+                    if (item.sellIn == 5) {
+                        item.quality += 3;
+                    }
+                    if (item.sellIn > 10) {
+                        item.quality += 1;
+                    }
+                    if (item.quality > 50) {
                         item.quality = 50;
                     }
-                }
-                item.sellIn = item.sellIn - 1;
-                if (item.sellIn < 0) {
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn == 0) {
                         item.quality = 0;
+                    }
+
+                    item.sellIn -= 1;
+                }else{
+                    if (!item.name.equals("Aged Brie")) {
+                        if (item.quality > 0) {
+                            item.quality = item.quality - 1;
+                        }
                     } else {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        } else {
+                            item.quality = 50;
+                        }
+                    }
+                    item.sellIn = item.sellIn - 1;
+                    if (item.sellIn < 0) {
                         if (item.name.equals("Aged Brie") && item.quality < 50) {
                             item.quality = item.quality + 1;
                         } else {
@@ -43,6 +51,7 @@ class GildedRose {
                                 item.quality = item.quality - 1;
                             }
                         }
+
                     }
                 }
             }
