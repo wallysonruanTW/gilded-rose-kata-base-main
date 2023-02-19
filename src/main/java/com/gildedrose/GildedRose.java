952ab9cf389b5376d4602f2c330511fa44321943
wallysonruanTW <wallyson.ruan@thoughtworks.com>
2023-02-19 00:43:09 -0300

    REFACTOR: Remoção de if e else desnecessarios

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -26,43 +26,35 @@ class GildedRose {
                 if (item.quality < 0){
                     item.quality = 0;
                 }
-            }else {
-                if (item.isLegendary(item.name)) {
-
+            }
+            if(item.isPassToConcert(item.name)){
+                if (item.sellIn > 5 && item.sellIn <= 10) {
+                    item.quality += 2;
                 }
-                else{
-                    if(item.isPassToConcert(item.name)){
-                        if (item.sellIn > 5 && item.sellIn <= 10) {
-                            item.quality += 2;
-                        }
-                        if (item.sellIn == 5) {
-                            item.quality += 3;
-                        }
-                        if (item.sellIn > 10) {
-                            item.quality += 1;
-                        }
-                        if (item.quality > 50) {
-                            item.quality = 50;
-                        }
-                        if (item.sellIn == 0) {
-                            item.quality = 0;
-                        }
+                if (item.sellIn == 5) {
+                    item.quality += 3;
+                }
+                if (item.sellIn > 10) {
+                    item.quality += 1;
+                }
+                if (item.quality > 50) {
+                    item.quality = 50;
+                }
+                if (item.sellIn == 0) {
+                    item.quality = 0;
+                }
 
-                        item.sellIn -= 1;
-                    }
-                    else{
-                        if (item.isBetterAged(item.name)){
-                            item.sellIn -= 1;
-                            if (item.sellIn < 0){
-                                item.quality += 2;
-                            }else{
-                                item.quality += 1;
-                            }
-                            if (item.quality > 50){
-                                item.quality = 50;
-                            }
-                        }
-                    }
+                item.sellIn -= 1;
+            }
+            if (item.isBetterAged(item.name)){
+                item.sellIn -= 1;
+                if (item.sellIn < 0){
+                    item.quality += 2;
+                }else{
+                    item.quality += 1;
+                }
+                if (item.quality > 50){
+                    item.quality = 50;
                 }
             }
         }
